0782e854387f68074cbc366c3b83196a0b1cadee
Roberto Freire <roberto.freire@valtech.com>
2025-12-02 21:52:15 -0300

    variable with pointer

--- variablesvaluesandoperators/investiment_calculator.go
+++ variablesvaluesandoperators/investiment_calculator.go
@@ -9,6 +9,9 @@ func main() {
 	var investimentAmount float64 = 1000
 	const expectedReturnRate = 5.5
 	years := 10
+
+	fmt.Print("Type investiment amount\n")
+	fmt.Scan(&investimentAmount)
 	var futureValue = investimentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
 	fmt.Printf("Future value: %.2f\n", futureValue)
 }
