b5f3961453a1236c8a7c553ccd35312bfeb50df0
Roberto Freire <roberto.freire@valtech.com>
2025-12-02 21:46:55 -0300

    use const

--- variablesvaluesandoperators/investiment_calculator.go
+++ variablesvaluesandoperators/investiment_calculator.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var investimentAmount float64 = 1000
-	var expectedReturnRate = 5.5
+	const expectedReturnRate = 5.5
 	years := 10
 	var futureValue = investimentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
 	fmt.Printf("Future value: %.2f\n", futureValue)
