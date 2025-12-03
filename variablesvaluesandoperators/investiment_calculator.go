093c22517aa7cd37f986a7bf86980749ab5a0e32
Roberto Freire <roberto.freire@valtech.com>
2025-12-02 21:37:44 -0300

    add explicit type assignment

--- variablesvaluesandoperators/investiment_calculator.go
+++ variablesvaluesandoperators/investiment_calculator.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	var investimentAmount = 1000
+	var investimentAmount float64 = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
-	var futureValue = float64(investimentAmount) * math.Pow((1+expectedReturnRate/100), float64(years))
+	var futureValue = investimentAmount * math.Pow((1+expectedReturnRate/100), float64(years))
 	fmt.Printf("Future value: %.2f\n", futureValue)
 }
